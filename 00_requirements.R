74a54509187dc9d9dc80714679162d8e8d074324
smnsl <seominsoll@gmail.com>
2025-11-07 18:47:21 -0800

    Update 00_requirements.R

---
+++ 00_requirements.R
@@ -0,0 +1,12 @@
+# tidyverse
+if (!require(tidyverse)) {      ## If package is not installed  
+  install.packages("tidyverse") ## Install
+  library(tidyverse)            ## Load package to environment.
+}
+
+
+# ggplot2
+if (!require(ggplot2)) { 
+  install.packages("ggplot2")
+  library(ggplot2)
+}
