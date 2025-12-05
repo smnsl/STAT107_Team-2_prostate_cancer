869f8d51ca455830b5ab8675a0dfd59556ad76fa
Jennifer M. Cho <minseoc0512@gmail.com>
2025-12-04 20:49:56 -0800

    fixed regression

--- 00_requirements.R
+++ 00_requirements.R
@@ -4,16 +4,11 @@ if (!require(tidyverse)) {      ## If package is not installed
   library(tidyverse)            ## Load package to environment.
 }
 
-# knitr
-if (!require(knitr)) {      
-  install.packages("knitr") 
-  library(knitr)            
-}
 
-# kableExtra
-if (!require(kableExtra)) {      
-  install.packages("kableExtra") 
-  library(kableExtra)            
+#GGally
+if (!require(GGally)) {      
+  install.packages("GGally") 
+  library(GGally)            
 }
 
 
